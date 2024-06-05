f612151d52289c66c0b24396476026f8971a4fbf
Kroese <kroese@users.noreply.github.com>
2024-06-05 11:56:58 +0200

    Update install.sh

--- src/install.sh
+++ src/install.sh
@@ -4,22 +4,23 @@ set -Eeuo pipefail
 # Docker environment variables
 : "${VERSION:="ventura"}"  # OSX Version
 
+TMP="$STORAGE/tmp"
 BASE_FILE="BaseSystem"
 BASE_IMG_ID="InstallMedia"
-BASE_IMG="$STORAGE/$BASE_FILE.img"
-BASE_DMG="$STORAGE/$BASE_FILE.dmg"
-BASE_TMP="$STORAGE/$BASE_FILE.tmp"
-BASE_CHK="$STORAGE/$BASE_FILE.chunklist"
+BASE_TMP="$TMP/$BASE_FILE.dmg"
+BASE_IMG="$STORAGE/$BASE_FILE.dmg"
 
 downloadImage() {
+
+  rm -rf "$TMP"
+  mkdir -p "$TMP"
 
   local msg="Downloading macOS ($VERSION) image"
   info "$msg..." && html "$msg..."
 
-  rm -f "$STORAGE/$BASE_FILE.*"
-  /run/progress.sh "$BASE_DMG" "" "$msg ([P])..." &
+  /run/progress.sh "$BASE_TMP" "" "$msg ([P])..." &
 
-  if ! /run/fetch-macOS-v2.py --action download -s "$VERSION" -n "$BASE_FILE" -o "$STORAGE"; then
+  if ! /run/fetch-macOS-v2.py --action download -s "$VERSION" -n "$BASE_FILE" -o "$TMP"; then
     error "Failed to fetch macOS ($VERSION)!"
     fKill "progress.sh"
     return 1
@@ -27,29 +28,22 @@ downloadImage() {
 
   fKill "progress.sh"
 
-  if [ ! -f "$BASE_DMG" ] || [ ! -s "$BASE_DMG" ]; then
-    error "Failed to find $BASE_DMG, aborting..."
+  if [ ! -f "$BASE_TMP" ] || [ ! -s "$BASE_TMP" ]; then
+    error "Failed to find file $BASE_TMP !"
     return 1
   fi
-
-  msg="Converting base image format..."
-  info "$msg" && html "$msg"
-
-  if ! dmg2img -i "$BASE_DMG" "$BASE_TMP"; then
-    error "Failed to convert base image format!"
-    return 1
-  fi
-
-  rm -f "$BASE_CHK"
-  rm -f "$BASE_DMG"
-  mv "$BASE_TMP" "$BASE_IMG"
 
   echo "$VERSION" > "$STORAGE/$PROCESS.version"
+
+  mv "$BASE_TMP" "$BASE_IMG"
+  rm -rf "$TMP"
+
+  return 0
 }
 
 if [ ! -f "$BASE_IMG" ] || [ ! -s "$BASE_IMG" ]; then
   if ! downloadImage; then
-    rm -f "$STORAGE/$BASE_FILE.*"
+    rm -rf "$TMP"
     exit 34
   fi
 fi
@@ -59,13 +53,12 @@ STORED_VERSION=$(cat "$STORAGE/$PROCESS.version")
 if [ "$VERSION" != "$STORED_VERSION" ]; then
   info "Different version detected, switching base image from $STORED_VERSION to $VERSION"
   if ! downloadImage; then
-    rm -f "$STORAGE/$BASE_FILE.*"
+    rm -rf "$TMP"
     exit 34
   fi
 fi
 
 DISK_OPTS="$DISK_OPTS -device virtio-blk-pci,drive=${BASE_IMG_ID},scsi=off,bus=pcie.0,addr=0x6,iothread=io2"
 DISK_OPTS="$DISK_OPTS -drive file=$BASE_IMG,id=$BASE_IMG_ID,format=raw,cache=$DISK_CACHE,aio=$DISK_IO,readonly=on,if=none"
-
 
 return 0
