226b575c3ba4b3ac1d92b34f5e8dcda6b6bcd4dc
Kroese <kroese@users.noreply.github.com>
2024-06-05 10:43:30 +0200

    Update install.sh

--- src/install.sh
+++ src/install.sh
@@ -9,22 +9,27 @@ BASE_IMG_ID="InstallMedia"
 BASE_IMG="$STORAGE/$BASE_FILE.img"
 BASE_DMG="$STORAGE/$BASE_FILE.dmg"
 BASE_TMP="$STORAGE/$BASE_FILE.tmp"
+BASE_CHK="$STORAGE/$BASE_FILE.chunklist"
 
 downloadImage() {
 
   rm -f "$BASE_DMG"
-  rm -f "$BASE_IMG"  
+  rm -f "$BASE_IMG"
   rm -f "$BASE_TMP"
 
-  local msg="Downloading macOS ($VERSION) image..."
-  info "$msg" && html "$msg"
+  local msg="Downloading macOS ($VERSION) image"
+  info "$msg..." && html "$msg..."
 
-  if ! /run/fetch-macOS-v2.py -s "$VERSION" -o "$STORAGE"; then
+  /run/progress.sh "$BASE_TMP" "" "$msg ([P])..." &
+
+  if ! /run/fetch-macOS-v2.py --action download -s "$VERSION" -n "$BASE_FILE" -o "$STORAGE"; then
     error "Failed to fetch macOS ($VERSION)!"
-    rm -f "$BASE_DMG"
+    fKill "progress.sh"
     return 1
   fi
 
+  fKill "progress.sh"
+  
   if [ ! -f "$BASE_DMG" ] || [ ! -s "$BASE_DMG" ]; then
     error "Failed to find $BASE_DMG, aborting..."
     return 1
@@ -35,11 +40,10 @@ downloadImage() {
 
   if ! dmg2img -i "$BASE_DMG" "$BASE_TMP"; then
     error "Failed to convert base image format!"
-    rm -f "$BASE_TMP"
-    rm -f "$BASE_DMG"
     return 1
   fi
 
+  rm -f "$BASE_CHK"
   rm -f "$BASE_DMG"
   mv "$BASE_TMP" "$BASE_IMG"
 
@@ -47,14 +51,20 @@ downloadImage() {
 }
 
 if [ ! -f "$BASE_IMG" ] || [ ! -s "$BASE_IMG" ]; then
-  ! downloadImage && exit 34
+  if ! downloadImage; then
+    rm -f "$STORAGE/$BASE_FILE.*"
+    exit 34
+  fi
 fi
 
 STORED_VERSION=$(cat "$STORAGE/$PROCESS.version")
 
 if [ "$VERSION" != "$STORED_VERSION" ]; then
   info "Different version detected, switching base image from $STORED_VERSION to $VERSION"
-  ! downloadImage && exit 34
+  if ! downloadImage; then
+    rm -f "$STORAGE/$BASE_FILE.*"
+    exit 34
+  fi
 fi
 
 DISK_OPTS="$DISK_OPTS -device virtio-blk-pci,drive=${BASE_IMG_ID},scsi=off,bus=pcie.0,addr=0x6,iothread=io2"
