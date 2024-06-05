488c710c07154f0bc86cb26d8cf6923b79571a34
Kroese <kroese@users.noreply.github.com>
2024-06-05 12:24:37 +0200

    Update install.sh

--- src/install.sh
+++ src/install.sh
@@ -59,6 +59,6 @@ if [ "$VERSION" != "$STORED_VERSION" ]; then
 fi
 
 DISK_OPTS="$DISK_OPTS -device virtio-blk-pci,drive=${BASE_IMG_ID},scsi=off,bus=pcie.0,addr=0x6,iothread=io2"
-DISK_OPTS="$DISK_OPTS -drive file=$BASE_IMG,id=$BASE_IMG_ID,format=raw,cache=$DISK_CACHE,aio=$DISK_IO,readonly=on,if=none"
+DISK_OPTS="$DISK_OPTS -drive file=$BASE_IMG,id=$BASE_IMG_ID,format=dmg,cache=$DISK_CACHE,aio=$DISK_IO,readonly=on,if=none"
 
 return 0
