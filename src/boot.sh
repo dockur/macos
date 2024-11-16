7e1c6fa5e3a3a051f2a08c488b8c76d23353445c
Kroese <kroese@users.noreply.github.com>
2024-11-16 10:40:16 +0100

    Update boot.sh

--- src/boot.sh
+++ src/boot.sh
@@ -88,7 +88,6 @@ sed -r -i -e 's|<string>iMacPro1,1</string>|<string>'"${MODEL}"'</string>|g' "$O
 sed -r -i -e 's|<string>007076A6-F2A2-4461-BBE5-BAD019F8025A</string>|<string>'"${UUID}"'</string>|g' "$OUT/EFI/OC/config.plist"
 sed -r -i -e 's|<string>C02TM2ZBHX87</string>|<string>'"${SN}"'</string>|g' "$OUT/EFI/OC/config.plist"
 sed -r -i -e 's|<string>C02717306J9JG361M</string>|<string>'"${MLB}"'</string>|g' "$OUT/EFI/OC/config.plist"
-sed -r -i -e 's|<string></string>|<string>'"${MODEL}"'</string>|g' "$OUT/EFI/OC/config.plist"
 sed -r -i -e 's|<data>m7zhIYfl</data>|<data>'"${ROM}"'</data>|g' "$OUT/EFI/OC/config.plist"
 
 cat "$OUT/EFI/OC/config.plist"
