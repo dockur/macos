f192cc9d7a3fe24a4776a1c6b197a2dd24ec2201
Kroese <kroese@users.noreply.github.com>
2025-03-16 11:09:52 +0100

    Update install.sh

--- src/install.sh
+++ src/install.sh
@@ -17,6 +17,89 @@ BASE_IMG_ID="InstallMedia"
 BASE_IMG="$STORAGE/base.dmg"
 BASE_VERSION="$STORAGE/$PROCESS.version"
 
+downloadFile() {
+
+  local url="$1"
+  local dest="$2"
+  local version="$3"
+  local msg rc total size progress
+
+  # Check if running with interactive TTY or redirected to docker log
+  if [ -t 1 ]; then
+    progress="--progress=bar:noscroll"
+  else
+    progress="--progress=dot:giga"
+  fi
+
+  rm -f "$dest"
+  msg="Downloading macOS ${version^}"
+  info "$msg recovery image..." && html "$msg..."
+
+  /run/progress.sh "$dest" "0" "$msg ([P])..." &
+
+  { wget "$url" -O "$dest" -q --timeout=30 --no-http-keep-alive --show-progress "$progress"; rc=$?; } || :
+
+  fKill "progress.sh"
+
+  if (( rc == 0 )) && [ -f "$dest" ]; then
+    total=$(stat -c%s "$dest")
+    size=$(formatBytes "$total")
+    if [ "$total" -lt 100000 ]; then
+      error "Invalid recovery image, file is only $size ?" && return 1
+    fi
+    html "Download finished successfully..."
+    return 0
+  fi
+
+  msg="Failed to download $url"
+  (( rc == 3 )) && error "$msg , cannot write file (disk full?)" && return 1
+  (( rc == 4 )) && error "$msg , network failure!" && return 1
+  (( rc == 8 )) && error "$msg , server issued an error response!" && return 1
+
+  error "$msg , reason: $rc"
+  return 1
+}
+
+function getRandom() {
+  local length="${1}"
+  local result=""
+  local chars=("0" "1" "2" "3" "4" "5" "6" "7" "8" "9" "A" "B" "C" "D" "E" "F")
+
+  for ((i=0; i<length; i++)); do
+      result+="${chars[$((RANDOM % 16))]}"
+  done
+
+  echo "$result"
+  return 0
+}
+
+function getLink() {
+  local info=""
+  local board="$1"
+  local type="latest"
+  local appleSession=""
+  local downloadLink=""
+  local downloadSession=""
+  local mlb="00000000000000000"
+    
+  appleSession=$(curl --disable -v -H "Host: osrecovery.apple.com" \
+                           -H "Connection: close" \
+                           -A "InternetRecovery/1.0" https://osrecovery.apple.com/ 2>&1 | tr ';' '\n' | awk -F'session=|;' '{print $2}' | grep 1)
+  info=$(curl --disable -s -X POST -H "Host: osrecovery.apple.com" \
+                           -H "Connection: close" \
+                           -A "InternetRecovery/1.0" \
+                           -b "session=\"${appleSession}\"" \
+                           -H "Content-Type: text/plain" \
+                           -d $'cid='"$(getRandom 16)"$'\nsn='${mlb}$'\nbid='${board}$'\nk='"$(getRandom 64)"$'\nfg='"$(getRandom 64)"$'\nos='${type} \
+                           https://osrecovery.apple.com/InstallationPayload/RecoveryImage | tr ' ' '\n')
+                           
+  downloadLink=$(echo "$info" | grep 'oscdn' | grep 'dmg')
+  downloadSession=$(echo "$info" | grep 'expires' | grep 'dmg')
+    
+  echo "${downloadLink}"
+  return 0
+}
+
 downloadImage() {
 
   local board
@@ -42,25 +125,11 @@ downloadImage() {
       return 1 ;;
   esac
 
-  local msg="Downloading macOS ${version^}"
-  info "$msg recovery image..." && html "$msg..."
-
   rm -rf "$TMP"
   mkdir -p "$TMP"
 
-  /run/progress.sh "$path" "" "$msg ([P])..." &
-
-  if ! /run/fetch.py -b "$board" -n "$file" -os latest -o "$TMP" download; then
-    error "Failed to fetch macOS \"${version^}\" recovery image with board id \"$board\"!"
-    fKill "progress.sh"
-    return 1
-  fi
-
-  fKill "progress.sh"
-
-  if [ ! -f "$path" ] || [ ! -s "$path" ]; then
-    error "Failed to find file \"$path\" !"
-    return 1
+  if ! downloadFile "$BOOT" "$path" "$version"; then
+    rm -rf "$TMP" && exit 60
   fi
 
   mv -f "$path" "$BASE_IMG"
@@ -130,8 +199,7 @@ generateSerial() {
 
 if [ ! -f "$BASE_IMG" ] || [ ! -s "$BASE_IMG" ]; then
   if ! downloadImage "$VERSION"; then
-    rm -rf "$TMP"
-    exit 34
+    rm -rf "$TMP" && exit 34
   fi
 fi
 
@@ -153,7 +221,7 @@ if ! generateID; then
 fi
 
 if ! generateSerial; then
-  error "Failed to generate serialnumber!" && exit 36
+  error "Failed to generate serial number!" && exit 36
 fi
 
 if ! generateAddress; then
