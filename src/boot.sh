76c66c2686cd51c3a358416aacc7f3db14567aa1
Kroese <kroese@users.noreply.github.com>
2026-07-05 21:53:27 +0200

    feat: Refactor boot code (#448)

--- src/boot.sh
+++ src/boot.sh
@@ -10,41 +10,36 @@ BOOT_DESC=""
 BOOT_OPTS=""
 OVMF="/usr/share/OVMF"
 
-msg="Configuring boot..."
-html "$msg"
-enabled "$DEBUG" && echo "$msg"
+selectOvmfFiles() {
+  case "${HEIGHT,,}" in
+    "1080" )
+      DEST="$PROCESS"
+      ROM="OVMF_CODE.fd"
+      VARS="OVMF_VARS-1920x1080.fd"
+      ;;
+    "768" )
+      DEST="${PROCESS}_hd"
+      ROM="OVMF_CODE.fd"
+      VARS="OVMF_VARS-1024x768.fd"
+      ;;
+    *)
+      ROM="OVMF_CODE.fd"
+      VARS="OVMF_VARS.fd"
+      DEST="${PROCESS}_${HEIGHT}"
+      ;;
+  esac
 
-case "${HEIGHT,,}" in
-  "1080" )
-    DEST="$PROCESS"
-    ROM="OVMF_CODE.fd"
-    VARS="OVMF_VARS-1920x1080.fd"
-    ;;
-  "768" )
-    DEST="${PROCESS}_hd"
-    ROM="OVMF_CODE.fd"
-    VARS="OVMF_VARS-1024x768.fd"
-    ;;
-  *)
-    ROM="OVMF_CODE.fd"
-    VARS="OVMF_VARS.fd"
-    DEST="${PROCESS}_${HEIGHT}"
-    ;;
-esac
+  DEST="$STORAGE/$DEST"
+  return 0
+}
 
-BOOT_OPTS+=" -smbios type=2"
-BOOT_OPTS+=" -rtc base=utc,base=localtime"
-BOOT_OPTS+=" -global ICH9-LPC.disable_s3=1"
-BOOT_OPTS+=" -global ICH9-LPC.disable_s4=1"
-BOOT_OPTS+=" -global ICH9-LPC.acpi-pci-hotplug-with-bridge-support=off"
+prepareOvmfRom() {
+  local logo
 
-osk=$(echo "bheuneqjbexolgurfrjbeqfthneqrqcyrnfrqbagfgrny(p)NccyrPbzchgreVap" | tr 'A-Za-z' 'N-ZA-Mn-za-m')
-BOOT_OPTS+=" -device isa-applesmc,osk=$osk"
+  if [ -s "$DEST.rom" ]; then
+    return 0
+  fi
 
-# OVMF
-DEST="$STORAGE/$DEST"
-
-if [ ! -s "$DEST.rom" ]; then
   [ ! -s "$OVMF/$ROM" ] && error "UEFI boot file ($OVMF/$ROM) not found!" && exit 44
 
   logo="/var/www/img/${PROCESS,,}.ffs"
@@ -59,28 +54,35 @@ if [ ! -s "$DEST.rom" ]; then
       cp "$OVMF/$ROM" "$DEST.tmp"
     fi
   fi
+
   mv "$DEST.tmp" "$DEST.rom"
   ! setOwner "$DEST.rom" && error "Failed to set the owner for \"$DEST.rom\" !"
-fi
 
-if [ ! -s "$DEST.vars" ]; then
+  return 0
+}
+
+prepareOvmfVars() {
+  if [ -s "$DEST.vars" ]; then
+    return 0
+  fi
+
   [ ! -s "$OVMF/$VARS" ] && error "UEFI vars file ($OVMF/$VARS) not found!" && exit 45
+
   cp "$OVMF/$VARS" "$DEST.tmp"
   mv "$DEST.tmp" "$DEST.vars"
   ! setOwner "$DEST.vars" && error "Failed to set the owner for \"$DEST.vars\" !"
-fi
 
-BOOT_OPTS+=" -drive if=pflash,format=raw,readonly=on,file=$DEST.rom"
-BOOT_OPTS+=" -drive if=pflash,format=raw,file=$DEST.vars"
+  return 0
+}
 
-IMG="$STORAGE/boot.img"
+addOvmfOptions() {
+  BOOT_OPTS+=" -drive if=pflash,format=raw,readonly=on,file=$DEST.rom"
+  BOOT_OPTS+=" -drive if=pflash,format=raw,file=$DEST.vars"
 
-if [ ! -f "$IMG" ]; then
+  return 0
+}
 
-  FILE="OpenCore.img"
-  IMG="/tmp/$FILE"
-  rm -f "$IMG"
-
+extractOpenCore() {
   # OpenCoreBoot
   ISO="/opencore.iso"
   OUT="/tmp/extract"
@@ -105,6 +107,15 @@ if [ ! -f "$IMG" ]; then
   [ -z "${CFG:-}" ] && error "Could not locate extracted OpenCore config.plist under \"$OUT\"." && exit 12
 
   EFI_DIR="${CFG%/OC/config.plist}"
+
+  return 0
+}
+
+configureOpenCorePlist() {
+  local brom
+  local plist
+  local resolution
+
   PLIST="/assets/config.plist"
   [ -f "/custom.plist" ] && PLIST="/custom.plist"
 
@@ -113,14 +124,14 @@ if [ ! -f "$IMG" ]; then
   # Replace placeholders with machine details
   ROM="${MAC//[^[:alnum:]]/}"
   ROM="${ROM,,}"
-  BROM=$(echo "$ROM" | xxd -r -p | base64)
-  RESOLUTION="${WIDTH}x${HEIGHT}@32"
+  brom=$(echo "$ROM" | xxd -r -p | base64)
+  resolution="${WIDTH}x${HEIGHT}@32"
 
-  sed -r -i -e 's|<data>ESIzRFVm</data>|<data>'"${BROM}"'</data>|g' "$CFG"
+  sed -r -i -e 's|<data>ESIzRFVm</data>|<data>'"${brom}"'</data>|g' "$CFG"
   sed -r -i -e 's|<string>iMac19,1</string>|<string>'"${MODEL}"'</string>|g' "$CFG"
   sed -r -i -e 's|<string>W00000000001</string>|<string>'"${SN}"'</string>|g' "$CFG"
   sed -r -i -e 's|<string>M0000000000000001</string>|<string>'"${MLB}"'</string>|g' "$CFG"
-  sed -r -i -e 's|<string>1920x1080@32</string>|<string>'"${RESOLUTION}"'</string>|g' "$CFG"
+  sed -r -i -e 's|<string>1920x1080@32</string>|<string>'"${resolution}"'</string>|g' "$CFG"
   sed -r -i -e 's|<string>00000000-0000-0000-0000-000000000000</string>|<string>'"${UUID}"'</string>|g' "$CFG"
 
   # Show boot picker if requested
@@ -130,6 +141,12 @@ if [ ! -f "$IMG" ]; then
     sed -i '/<key>Timeout<\/key>/{n;s/<integer>[0-9]\+<\/integer>/<integer>10<\/integer>/}' "$CFG"
     sed -i '/<key>PickerMode<\/key>/{n;s/<string>External<\/string>/<string>Builtin<\/string>/}' "$CFG"
   fi
+
+  return 0
+}
+
+addVmHideKext() {
+  local kexts
 
   # Add kext to disable VM detection
   kexts="$EFI_DIR/OC/Kexts"
@@ -141,6 +158,10 @@ if [ ! -f "$IMG" ]; then
   mv "$OUT/kext/VMHide.kext" "$kexts"
   rm -rf "$OUT/kext"
 
+  return 0
+}
+
+buildOpenCoreImage() {
   # Build image
 
   MB=256
@@ -162,15 +183,15 @@ if [ ! -f "$IMG" ]; then
 
   PART="/tmp/partition.fdisk"
 
-  {       echo "label: gpt"
-          echo "label-id: 1ACB1E00-3B8F-4B2A-86A4-D99ED21DCAEB"
-          echo "device: $FILE"
-          echo "unit: sectors"
-          echo "first-lba: $FIRST_LBA"
-          echo "last-lba: $LAST_LBA"
-          echo "sector-size: $SECTOR"
-          echo ""
-          echo "${FILE}1 : start=$START, size=$COUNT, type=C12A7328-F81F-11D2-BA4B-00A0C93EC93B, uuid=05157F6E-0AE8-4D1A-BEA5-AC172453D02C, name=\"primary\""
+  {   echo "label: gpt"
+      echo "label-id: 1ACB1E00-3B8F-4B2A-86A4-D99ED21DCAEB"
+      echo "device: $FILE"
+      echo "unit: sectors"
+      echo "first-lba: $FIRST_LBA"
+      echo "last-lba: $LAST_LBA"
+      echo "sector-size: $SECTOR"
+      echo ""
+      echo "${FILE}1 : start=$START, size=$COUNT, type=C12A7328-F81F-11D2-BA4B-00A0C93EC93B, uuid=05157F6E-0AE8-4D1A-BEA5-AC172453D02C, name=\"primary\""
 
   } > "$PART"
 
@@ -182,6 +203,10 @@ if [ ! -f "$IMG" ]; then
 
   rm -rf "$OUT"
 
+  return 0
+}
+
+printMachineDetails() {
   info ""
   info "Model: $MODEL"
   info "Rom: $ROM"
@@ -189,7 +214,50 @@ if [ ! -f "$IMG" ]; then
   info "Board: $MLB"
   info ""
 
-fi
+  return 0
+}
+
+prepareOpenCoreImage() {
+  IMG="$STORAGE/boot.img"
+
+  if [ -f "$IMG" ]; then
+    return 0
+  fi
+
+  FILE="OpenCore.img"
+  IMG="/tmp/$FILE"
+  rm -f "$IMG"
+
+  extractOpenCore
+  configureOpenCorePlist
+  addVmHideKext
+  buildOpenCoreImage
+  printMachineDetails
+
+  return 0
+}
+
+msg="Configuring boot..."
+html "$msg"
+enabled "$DEBUG" && echo "$msg"
+
+selectOvmfFiles
+
+BOOT_OPTS+=" -smbios type=2"
+BOOT_OPTS+=" -rtc base=utc,base=localtime"
+BOOT_OPTS+=" -global ICH9-LPC.disable_s3=1"
+BOOT_OPTS+=" -global ICH9-LPC.disable_s4=1"
+BOOT_OPTS+=" -global ICH9-LPC.acpi-pci-hotplug-with-bridge-support=off"
+
+osk=$(echo "bheuneqjbexolgurfrjbeqfthneqrqcyrnfrqbagfgrny(p)NccyrPbzchgreVap" | tr 'A-Za-z' 'N-ZA-Mn-za-m')
+BOOT_OPTS+=" -device isa-applesmc,osk=$osk"
+
+# OVMF
+prepareOvmfRom
+prepareOvmfVars
+addOvmfOptions
+
+prepareOpenCoreImage
 
 ! setOwner "$IMG" && error "Failed to set the owner for \"$IMG\" !"
 
